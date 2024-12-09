d416a19137bd955be03b8a5cfcaf1099192910ef
parkminsung <parkminsung>
2024-12-09 14:55:08 +0900

    WebSecurityConfig 수정 일부 롤백

--- project2/src/main/java/com/lec/project/WebSecurityConfig.java
+++ project2/src/main/java/com/lec/project/WebSecurityConfig.java
@@ -1,7 +1,6 @@
 package com.lec.project;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -10,15 +9,16 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 
 @Log4j2
-@Configuration
+// @Configuration
 @EnableMethodSecurity(prePostEnabled = true)
 @RequiredArgsConstructor
 public class WebSecurityConfig {
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		http.authorizeHttpRequests((authorizeRequests) -> authorizeRequests.anyRequest().permitAll()).formLogin((formLogin) -> formLogin.usernameParameter("username")
-																																		.passwordParameter("password")
-																																		.defaultSuccessUrl("/", true));
+		http.authorizeHttpRequests((authorizeRequests) -> authorizeRequests.anyRequest().permitAll())
+																						.formLogin((formLogin) -> formLogin.usernameParameter("username")
+																														   .passwordParameter("password")
+																														   .defaultSuccessUrl("/", true));
         http.csrf(csrf -> csrf.disable());
 		
 		return http.build();
