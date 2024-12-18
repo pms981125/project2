b2d9d9ec6d27e08eeb8e6ad41b309d3ef40cdb1e
sungil4651 <Administrator@User -2023ULJSU>
2024-12-18 12:49:17 +0900

    yml 변경 test...

--- project2/src/main/java/com/lec/project/shoppingmall/repository/search/ShopSearchImpl.java
+++ project2/src/main/java/com/lec/project/shoppingmall/repository/search/ShopSearchImpl.java
@@ -3,11 +3,13 @@ package com.lec.project.shoppingmall.repository.search;
 import java.util.List;
 
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
 
 import com.lec.project.shoppingmall.domain.QShop;
 import com.lec.project.shoppingmall.domain.Shop;
+import com.querydsl.core.BooleanBuilder;
 import com.querydsl.jpa.JPQLQuery;
 
 import lombok.extern.log4j.Log4j2;
@@ -20,49 +22,22 @@ public class ShopSearchImpl extends QuerydslRepositorySupport implements ShopSea
 	}
 
 	@Override
-	public Page<Shop> searchLike(Pageable pageable) {
-		
-		QShop shop = QShop.shop;
-		JPQLQuery<Shop> query = from(shop);		// select * from shop;
-		query.where(shop.gName.contains("1"));	// where title like '%1%';
-		List<Shop> list = query.fetch();
-		long count = query.fetchCount();
-		log.info("검색건수 = " + count);
-		return null;
-	}
-
-	@Override
-	public Page<Shop> searchPageable(Pageable pageable) {
-		
+	public Page<Shop> searchAllImpl(String keyword, Pageable pageable) {
 		QShop shop = QShop.shop;
 		JPQLQuery<Shop> query = from(shop);
-		query.where(shop.gName.contains("1"))
-			.limit(1)
-			.offset(2);
+		
+		if(keyword != null) {
+			BooleanBuilder booleanBuilder = new BooleanBuilder();
+			booleanBuilder.or(shop.gName.contains(keyword)); // 상품이름으로 검색
+			
+			query.where(booleanBuilder);
+		}
 		
 		this.getQuerydsl().applyPagination(pageable, query);
 		List<Shop> list = query.fetch();
-		long count = query.limit(1).offset(2).fetchCount();
-		log.info("검색건수 = " + count);
-		return null;
-	}
-
-	@Override
-	public Page<Shop> searchBooleanBuilder(Pageable pageable) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Page<Shop> searchAll(String keyword, Pageable pageable) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Page<Shop> searchAllImpl(String keywString, Pageable pageable) {
-		// TODO Auto-generated method stub
-		return null;
+		long count = query.fetchCount();
+		
+		return new PageImpl<>(list, pageable, count);
 	}
 
 }
