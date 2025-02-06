34725aca45bb4f74e7f009ba0dc83ed16731bae9
sungil4651 <oksungil123@gmail.com>
2025-02-06 18:09:43 +0900

    webClientConfig기능 webConfig으로 통합 및 webClientConfig 삭제

--- project2/src/main/java/com/lec/project/WebConfig.java
+++ project2/src/main/java/com/lec/project/WebConfig.java
@@ -1,6 +1,8 @@
 package com.lec.project;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -13,5 +15,12 @@ public class WebConfig implements WebMvcConfigurer {
     	registry.addResourceHandler("/uploads/**")
         		.addResourceLocations("file:/D:/sungil/98.temp/upload/");
 
+    }
+    
+    @Bean
+    public WebClient webClient() {
+        return WebClient.builder()
+            .baseUrl("https://kapi.kakao.com") // 카카오페이 API 기본 URL
+            .build();
     }
 }
