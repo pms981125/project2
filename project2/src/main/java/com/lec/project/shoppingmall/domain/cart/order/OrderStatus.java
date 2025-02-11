3af1ce52b73e715eb0bcc82f198c9bfc1e41d459
parkminsung <pms981125@naver.com>
2025-02-11 18:15:53 +0900

    enum 에러 수정

--- project2/src/main/java/com/lec/project/shoppingmall/domain/cart/order/OrderStatus.java
+++ project2/src/main/java/com/lec/project/shoppingmall/domain/cart/order/OrderStatus.java
@@ -4,7 +4,9 @@ public enum OrderStatus {
     PENDING("대기"),
     APPROVED("승인"),
     REJECTED("거절"),
-	REFUND_REQUESTED("환불 요청");
+	REFUND_REQUESTED("환불 요청"),
+	REFUND_SUCCESS("환불 완료"),
+    REFUND_REJECTED("환불 거절");
 	
     private final String description;
     
