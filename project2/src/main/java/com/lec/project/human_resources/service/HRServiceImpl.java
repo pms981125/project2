06f60e9441433a459d29ce2f9f8ce64fff355a61
parkminsung <pms981125@naver.com>
2025-02-17 10:13:20 +0900

    회원 정보 수정4

--- project2/src/main/java/com/lec/project/human_resources/service/HRServiceImpl.java
+++ project2/src/main/java/com/lec/project/human_resources/service/HRServiceImpl.java
@@ -39,13 +39,17 @@ import com.lec.project.human_resources.dto.CalendarDTO;
 import com.lec.project.human_resources.repository.AdminRepository;
 import com.lec.project.human_resources.repository.AttendanceRepository;
 import com.lec.project.human_resources.repository.WorkLogRepository;
+import com.lec.project.shoppingmall.domain.cart.Cart;
 import com.lec.project.shoppingmall.domain.cart.order.Ordered;
 import com.lec.project.shoppingmall.domain.payment.kakao.KakaoPayment;
 import com.lec.project.shoppingmall.domain.refund.Refund;
+import com.lec.project.shoppingmall.repository.CartRepository;
 import com.lec.project.shoppingmall.repository.KakaoPaymentRepository;
 import com.lec.project.shoppingmall.repository.OrderedRepository;
 import com.lec.project.shoppingmall.repository.RefundRepository;
+import com.lec.project.shoppingmall.service.cart.CartService;
 
+import groovyjarjarantlr4.v4.parse.ANTLRParser.finallyClause_return;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
@@ -62,6 +66,8 @@ public class HRServiceImpl implements HRService {
 	private final OrderedRepository orderedRepository;
 	private final KakaoPaymentRepository kakaoPaymentRepository;
 	private final RefundRepository refundRepository;
+	private final CartRepository cartRepository;
+	private final CartService cartService;
 	private final PasswordEncoder passwordEncoder;
 
 	/*	@Override
@@ -170,6 +176,7 @@ public class HRServiceImpl implements HRService {
 		List<Ordered> orderedList = orderedRepository.findByMemberOrderByOrderDateDesc(member);
 		List<KakaoPayment> paymentList = kakaoPaymentRepository.findPaymentByID(id);
 		List<Refund> refundList = refundRepository.findByMember(member);
+		List<Cart> cartList = cartRepository.findAllByMember(member);
 		
 		if (isAdmin) {
 			adminRepository.deleteById(id);
@@ -185,6 +192,12 @@ public class HRServiceImpl implements HRService {
 		
 		for (Refund refund : refundList) {
 			refundRepository.delete(refund);
+		}
+		
+		cartService.removeAll(id, true);
+		
+		for (Cart cart : cartList) {
+			cartRepository.delete(cart);
 		}
 		
 		memberRepository.deleteById(id);
--- project2/src/main/java/com/lec/project/shoppingmall/repository/CartRepository.java
+++ project2/src/main/java/com/lec/project/shoppingmall/repository/CartRepository.java
@@ -1,12 +1,19 @@
 package com.lec.project.shoppingmall.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import com.lec.project.Member;
 import com.lec.project.shoppingmall.domain.cart.Cart;
 
 
 public interface CartRepository extends JpaRepository<Cart, Long>{
 	Optional<Cart> findByMemberId(String memberId);
+
+	@Query("SELECT c FROM Cart c WHERE c.member = :member")
+	List<Cart> findAllByMember(@Param("member") Member member);
 }
